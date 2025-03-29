1d3bbb7a6780f482d8adda19e8d662af312edf9e
anna <bohachenko.anna@gmail.com>
2025-03-29 18:08:58 +0100

    adding jsBasics folder with resolved tasks for lesson4

---
+++ JS/jsBasics/task1.js
@@ -0,0 +1,37 @@
+﻿'use strict'
+
+//string
+
+let thisIsString = "пу пу пу";
+
+//number
+
+let thisIsNumber = 112358;
+
+//bigint
+
+let ThisIsBigInt = 1234567890123456789012345678901234567890n;
+
+//null
+
+let thisIsNull = null;
+
+//boolean
+
+let thisIsBoolean = true;
+
+//underfined
+
+let thisIsUndefined = undefined;
+
+//symbol
+
+let thisIsSymbol = Symbol('id');
+
+console.log(thisIsString);
+console.log(thisIsNumber);
+console.log(ThisIsBigInt);
+console.log(thisIsNull);
+console.log(thisIsBoolean);
+console.log(thisIsUndefined);
+console.log(thisIsSymbol);
